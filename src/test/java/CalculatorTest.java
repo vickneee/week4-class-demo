d74190aa53ac60dddc7405e4fa8bcef409a757c1
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-09 13:58:29 +0300

    Update tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,46 +1,61 @@
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
-    Calculator cal = new Calculator();
+    private Calculator calculator;
 
+    // Runs ONCE before all tests
     @BeforeAll
-    static void setup() {
+    static void setupAll() {
         System.out.println("Starting Calculator Tests");
     }
 
+    // Runs BEFORE EACH test
     @BeforeEach
-    void init() {
-        System.out.println("Starting a test");
+    void setup() {
+        calculator = new Calculator();
+        System.out.println(">>> @BeforeEach: Starting a test");
     }
 
+    // A TEST method
     @Test
     void add() {
-        assertEquals(5, cal.add(2, 3));
+        assertEquals(5, calculator.add(2, 3));
     }
 
     @Test
     void sub() {
-        assertEquals(1, cal.sub(3, 2));
+        assertEquals(1, calculator.sub(3, 2));
     }
 
     @Test
     void mul() {
-        assertEquals(6, cal.mul(2, 3));
+        assertEquals(6, calculator.mul(2, 3));
     }
 
     @Test
     void div() {
-        assertEquals(2.0, cal.div(6, 3));
+        assertEquals(2.0, calculator.div(6, 3));
     }
 
+    @Test
+    void divByZero() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculator.div(1, 0);
+        });
+    }
+
+    // Runs AFTER EACH test
+    @AfterEach
+    void teardownEach() {
+        System.out.println(">>> @AfterEach: Test completed");
+    }
+
+    // Runs ONCE after all tests
     @AfterAll
-    static void teardown() {
+    static void teardownAll() {
         System.out.println("Calculator Tests Completed");
     }
 }
